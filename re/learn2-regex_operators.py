957496104af226418b8242119e72e379d39717ed
YusufSyam <54948391+YusufSyam@users.noreply.github.com>
2022-01-25 04:28:12 +0800

    Update learn2-regex_operators.py

--- re/learn2-regex_operators.py
+++ re/learn2-regex_operators.py
@@ -95,7 +95,7 @@ string6= 'balapan di jalanan walaupun hujan'
 sol_6= re.findall(r'\w+la[nup]\w+', string6)
 print(sol_6)
 
-# ------------- Soal 6: ekstrak tanggal -------------
+# ------------- Soal 7: ekstrak tanggal -------------
 string7= 'Amit 34-3456 12-05-2007, XYZ 56-4532 11-11-2011, ABC 67-8945 12-01-2009'
 sol_7= re.findall(r'\d{2}-\d{2}-\d{4}', string7)
 print(sol_7)
