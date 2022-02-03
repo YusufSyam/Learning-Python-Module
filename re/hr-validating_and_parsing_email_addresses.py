14a15f5b554347c279119a3a542c999a5b87bdfb
Yusuf Syam <muhyusufsyamsyam@gmail.com>
2022-02-03 23:14:32 +0800

    Add files via upload

---
+++ re/hr-validating_and_parsing_email_addresses.py
@@ -0,0 +1,22 @@
+import re
+
+n= int(input())
+
+emails= [input() for i in range(n)]
+# emails= ['DEXTER <dexter@hotmail.com>', 'VIRUS <virus!@variable.:p>']
+# emails= '''shashank <shashank@9mail.com>
+# shashank <shashank@gmail.9om>
+# shashank <shashank@gma_il.com>
+# shashank <shashank@mail.moc>
+# shashank <shashank@company-mail.com>
+# shashank <shashank@companymail.c_o>'''.split('\n')
+
+valid_emails= []
+
+for i in emails:
+    res= re.search(r'\<[a-zA-Z][a-zA-Z0-9._-]*@[a-zA-Z]*\.[a-zA-Z]{1,3}>', i)
+    # print(res.group(0) if bool(res) else 'Not an email')
+    if(bool(res)):
+        valid_emails.append(i)
+
+print('\n'.join([i for i in valid_emails]))
